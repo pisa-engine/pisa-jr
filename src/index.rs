3cdc00a52f4b57ccfc036f5dfaa4df02dd53bc18
Antonio Mallia <me@antoniomallia.it>
2020-02-24 19:00:41 -0500

    Initial implementation of the indexer

--- src/index.rs
+++ src/index.rs
@@ -1,4 +1,9 @@
+extern crate libflate;
+
+use libflate::gzip::Decoder;
+use pisa_jr::parser;
 use std::path::PathBuf;
+use std::{fs, io};
 use structopt::StructOpt;
 
 #[derive(StructOpt, Debug)]
@@ -8,6 +13,16 @@ struct Opt {
 }
 
 #[paw::main]
-fn main(_opt: Opt) {
-    unimplemented!()
+fn main(opt: Opt) -> Result<(), io::Error> {
+    let mut entries = fs::read_dir(opt.input)?
+        .map(|res| res.map(|e| e.path()))
+        .map(|res| fs::File::open(res.unwrap()).expect("TODO"))
+        .map(|res| Decoder::new(res).unwrap())
+        .map(|res| io::BufReader::new(res))
+        .map(|res| parser::split_trec_records(res))
+        .map(|res| res.map(|bytes| String::from_utf8(bytes.unwrap())));
+
+    let records: Result<Vec<_>, _> = entries.next().unwrap().collect();
+    println!("{:?}", records.unwrap());
+    Ok(())
 }
