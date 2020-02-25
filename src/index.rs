3373489cf6c6c3782369d79fa2a49cb6f5369835
Michal Siedlaczek <michal.siedlaczek@nyu.edu>
2020-02-25 16:40:43 -0500

    Use external trec parser

--- src/index.rs
+++ src/index.rs
@@ -1,13 +1,35 @@
+use std::fs::File;
+use std::io::{BufReader, Read};
 use std::path::PathBuf;
 use structopt::StructOpt;
+use trec_text::Parser;
 
 #[derive(StructOpt, Debug)]
 struct Opt {
-    #[structopt(help = "Collection file in TREC format")]
-    input: PathBuf,
+    #[structopt(help = "Collection files in TREC format")]
+    input: Vec<PathBuf>,
 }
 
-#[paw::main]
-fn main(_opt: Opt) {
-    unimplemented!()
+fn main() {
+    let empty: Box<dyn Read> = Box::new(std::io::empty());
+    let input = Opt::from_args()
+        .input
+        .into_iter()
+        .map(|path| File::open(path).map(BufReader::new))
+        .fold(empty, |acc, f| match f {
+            Ok(f) => Box::new(acc.chain(f)),
+            Err(err) => {
+                eprintln!("Unable to read input file: {}. Skipping...", err);
+                Box::new(std::io::empty())
+            }
+        });
+    for document in Parser::new(input) {
+        if let Ok(doc) = document {
+            println!(
+                "{}: {}",
+                String::from_utf8_lossy(doc.docno()),
+                String::from_utf8_lossy(doc.content())
+            );
+        }
+    }
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,5 @@
+//! Minimalistic version of PISA written in Rust.
+
 #![warn(
     missing_docs,
     trivial_casts,
@@ -7,35 +9,3 @@
 )]
 #![warn(clippy::all, clippy::pedantic)]
 #![allow(clippy::module_name_repetitions)]
-//! Minimalistic version of PISA written in Rust.
-
-use std::iter::Peekable;
-
-#[derive(Debug, PartialEq, Eq)]
-pub struct Document {
-    pub docno: String,
-    pub content: String,
-}
-
-impl Document {
-    fn new<S: Into<String>>(docno: S, content: S) -> Self {
-        Self {
-            docno: docno.into(),
-            content: content.into(),
-        }
-    }
-}
-
-fn skip_whitespaces<B>(bytes: &mut Peekable<B>)
-where
-    B: Iterator<Item = std::io::Result<u8>>,
-{
-    while let Some(byte) = bytes.peek() {
-        if !byte.as_ref().map(u8::is_ascii_whitespace).unwrap_or(false) {
-            break;
-        }
-        bytes.next();
-    }
-}
-
-pub mod parser;
